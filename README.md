9ac9ab3dc4f1b4e558efa795cb8ee6d9665ae371
godJah <43356590+stgodjah@users.noreply.github.com>
2020-10-05 05:20:41 +0300

    added certificates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BitGanjVargant
  
+ node1 - Web server: Apache + php
+
+ node2 - DB Server:mysql  Ver 15.1 Distrib 10.3.24-MariaDB, for Linux (x86_64) 
--- bootstrap_db.sh
+++ bootstrap_db.sh
@@ -1,23 +1,7 @@
-# Install xrdp
-#--------------
-#apt-get -y install xrdp ubuntu-mate-desktop
-#systemctl enable xrdp
-#reboot
-
-# Install Webmin
+# Install Apache 
 #---------------
-
-#curl -L -O http://www.webmin.com/download/deb/webmin-current.deb
-#dpkg -i webmin-current.deb
-#echo "allow=127.0.0.1 192.168.2.0/24" >> /etc/webmin/miniserv.conf
-#echo "god:x:0" >> /etc/webmin/miniserv.users
-#systemctl restart webmin
-
-
-# Install Cockpit
-# ---------------
-#apt-get -y cockpit 
-#systemctl enable cockpit.socket
+apt-get -y install apache2
+chown -R god:www-data /var/www
 
 # Install php
 #------------
@@ -25,10 +9,15 @@ add-apt-repository -y ppa:ondrej/php
 apt-get -y install php7.4 
 apt-get -y install php-pear
 apt-get -y install php7.4-zip php7.4-bcmath php7.4-bz2 php7.4-intl php7.4-gd php7.4-mbstring php7.4-mysql php7.4-fpm php7.4-curl
-apt-get -y install php-dev
-pecl install xdebug
 cd /home/god
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 mv composer.phar /usr/local/bin/composer
 php -r "unlink('composer-setup.php');"
+
+a2ensite default-ssl
+a2enmod ssl
+systemctl restart apache2
+
+apt -y install mariadb-server
+apt -y install phpmyadmin php-gettext
