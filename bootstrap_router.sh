1c37343e130fb1a654c0030c008e1668e9af1580
godJah <43356590+stgodjah@users.noreply.github.com>
2020-11-03 23:16:35 +0200

    add lampp install to router

--- bootstrap_router.sh
+++ bootstrap_router.sh
@@ -4,3 +4,7 @@ systemctl start nginx
 systemctl enable nginx
 
 #apt -y install certbot
+cd /opt
+wget https://www.apachefriends.org/xampp-files/7.4.11/xampp-linux-x64-7.4.11-0-installer.run
+chmod 755 xampp-linux-x64-7.4.11-0-installer.run
+sudo ./xampp-linux-x64-7.4.11-0-installer.run --mode unattended
