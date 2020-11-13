41f5d2e1090dc6cabb5a49c4d2451099debd459a
godJah <43356590+stgodjah@users.noreply.github.com>
2020-11-13 13:19:23 +0200

    Update bootstrap_router.sh
    Install open vpn

--- bootstrap_router.sh
+++ bootstrap_router.sh
@@ -1,3 +1,11 @@
+### Install Open VPN
+apt install apt-transport-https
+wget https://swupdate.openvpn.net/repos/openvpn-repo-pkg-key.pub
+apt-key add openvpn-repo-pkg-key.pub
+wget -O /etc/apt/sources.list.d/openvpn3.list https://swupdate.openvpn.net/community/openvpn3/repos/openvpn3-bionic.list
+apt update
+apt -y install openvpn3
+
 ### Route web traffic with NGinx
 apt-get -y install nginx
 systemctl start nginx
